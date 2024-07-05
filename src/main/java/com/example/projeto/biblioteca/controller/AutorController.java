dfc42160686b7d959eb99f91a89016bab2563131
iuri <bentoiuri@gmail.com>
2024-07-05 09:43:39 -0300

    cria metodos em AutorController e service

--- src/main/java/com/example/projeto/biblioteca/controller/AutorController.java
+++ src/main/java/com/example/projeto/biblioteca/controller/AutorController.java
@@ -2,10 +2,13 @@ package com.example.projeto.biblioteca.controller;
 
 import com.example.projeto.biblioteca.dto.AutorRequest;
 import com.example.projeto.biblioteca.dto.AutorResponse;
+import com.example.projeto.biblioteca.model.Autor;
 import com.example.projeto.biblioteca.service.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("autor")
@@ -16,7 +19,27 @@ public class AutorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public AutorResponse salvar (@RequestBody AutorRequest autorRequest) {
+    public AutorResponse salvar(@RequestBody AutorRequest autorRequest) {
         return autorService.salvar(autorRequest);
+    }
+
+    @GetMapping
+    public List<AutorResponse> listar(){
+        return autorService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public AutorResponse pegarPorId(@PathVariable Integer id){
+        return AutorResponse.converter(autorService.pegarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public AutorResponse atualizar(@PathVariable Integer id, @RequestBody AutorRequest autorRequest){
+        return AutorResponse.converter(autorService.atualizar(id, autorRequest));
+    }
+
+    @DeleteMapping
+    public void deletar(Integer id){
+        autorService.deletar(id);
     }
 }
--- src/main/java/com/example/projeto/biblioteca/service/AutorService.java
+++ src/main/java/com/example/projeto/biblioteca/service/AutorService.java
@@ -4,8 +4,12 @@ import com.example.projeto.biblioteca.dto.AutorRequest;
 import com.example.projeto.biblioteca.dto.AutorResponse;
 import com.example.projeto.biblioteca.model.Autor;
 import com.example.projeto.biblioteca.repository.AutorRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import java.util.List;
 
 @Service
 public class AutorService {
@@ -13,8 +17,29 @@ public class AutorService {
     @Autowired
     private AutorRepository autorRepository;
 
-    public AutorResponse salvar (AutorRequest autorRequest){
+    public AutorResponse salvar(AutorRequest autorRequest){
         return AutorResponse.converter(autorRepository.save(Autor.converter(autorRequest)));
+    }
+
+    public List<AutorResponse> listar(){
+        return autorRepository.findAll().stream()
+                .map(AutorResponse::converter).toList();
+    }
+
+    public Autor pegarPorId(Integer id){
+        return autorRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Recurso não encontrado")
+        );
+    }
+
+    public Autor atualizar(Integer id, AutorRequest autorRequest){
+        var autorAtualizado = pegarPorId(id);
+        BeanUtils.copyProperties(autorRequest, autorAtualizado, "id");
+        return autorRepository.save(autorAtualizado);
+    }
+
+    public void deletar(Integer id){
+        autorRepository.deleteById(id);
     }
 
 }
