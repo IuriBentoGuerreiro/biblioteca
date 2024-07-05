42e4b86f98399862027913bef80ae6662190c209
iuri <bentoiuri@gmail.com>
2024-07-05 10:03:24 -0300

    implementa configuraçoes do swagger

--- src/main/java/com/example/projeto/biblioteca/controller/AutorController.java
+++ src/main/java/com/example/projeto/biblioteca/controller/AutorController.java
@@ -2,14 +2,15 @@ package com.example.projeto.biblioteca.controller;
 
 import com.example.projeto.biblioteca.dto.AutorRequest;
 import com.example.projeto.biblioteca.dto.AutorResponse;
-import com.example.projeto.biblioteca.model.Autor;
 import com.example.projeto.biblioteca.service.AutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Autor")
 @RestController
 @RequestMapping("autor")
 public class AutorController {
@@ -17,27 +18,31 @@ public class AutorController {
     @Autowired
     private AutorService autorService;
 
+    @Operation(summary = "Salvar")
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public AutorResponse salvar(@RequestBody AutorRequest autorRequest) {
         return autorService.salvar(autorRequest);
     }
 
+    @Operation(summary = "Listar")
     @GetMapping
     public List<AutorResponse> listar(){
         return autorService.listar();
     }
 
+    @Operation(summary = "Pegar por id")
     @GetMapping("/{id}")
     public AutorResponse pegarPorId(@PathVariable Integer id){
         return AutorResponse.converter(autorService.pegarPorId(id));
     }
 
+    @Operation(summary = "Atualizar")
     @PutMapping("/{id}")
     public AutorResponse atualizar(@PathVariable Integer id, @RequestBody AutorRequest autorRequest){
         return AutorResponse.converter(autorService.atualizar(id, autorRequest));
     }
 
+    @Operation(summary = "Deletar")
     @DeleteMapping("/{id}")
     public void deletar(Integer id){
         autorService.deletar(id);
--- src/main/java/com/example/projeto/biblioteca/controller/LivroController.java
+++ src/main/java/com/example/projeto/biblioteca/controller/LivroController.java
@@ -3,12 +3,14 @@ package com.example.projeto.biblioteca.controller;
 import com.example.projeto.biblioteca.dto.LivroRequest;
 import com.example.projeto.biblioteca.dto.LivroResponse;
 import com.example.projeto.biblioteca.service.LivroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Livro")
 @RestController
 @RequestMapping("biblioteca")
 public class LivroController {
@@ -16,27 +18,31 @@ public class LivroController {
     @Autowired
     private LivroService livroService;
 
+    @Operation(summary = "Salvar")
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public LivroResponse salvar (@RequestBody LivroRequest livroRequest){
         return livroService.salvar(livroRequest);
     }
 
+    @Operation(summary = "Listar")
     @GetMapping
     public List<LivroResponse> listarTodos(){
         return livroService.buscarTodos();
     }
 
+    @Operation(summary = "Pegar por id")
     @GetMapping("/{id}")
     public LivroResponse pegarPorId(@PathVariable Integer id){
         return LivroResponse.converter(livroService.pegarPorId(id));
     }
 
+    @Operation(summary = "Atualizar")
     @PutMapping("/{id}")
     public LivroResponse atualizar(@PathVariable Integer id, @RequestBody LivroRequest livroRequest){
         return LivroResponse.converter(livroService.atualizar(id, livroRequest));
     }
 
+    @Operation(summary = "Deletar")
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Integer id){
         livroService.deletar(id);
